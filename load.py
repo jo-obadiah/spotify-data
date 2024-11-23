33a289ba4e1d8c7210b21b4d39e8ef9487918a8d
Joel Obadiah <joelobadiahdelle@gmail.com>
2024-11-23 20:50:52 +0100

    load.py

---
+++ load.py
@@ -0,0 +1,12 @@
+import Extract
+import Transform
+import sqlalchemy
+import pandas as pd 
+from sqlalchemy.orm import sessionmaker
+import requests
+import json
+from datetime import datetime
+import datetime
+import sqlite3
+
+
