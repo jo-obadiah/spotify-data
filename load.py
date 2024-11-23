9e45b6a9aee48e8b816e52ac9f00cc21accbcfcb
Joel Obadiah <joelobadiahdelle@gmail.com>
2024-11-23 20:54:52 +0100

    changes

--- load.py
+++ load.py
@@ -10,3 +10,9 @@ import datetime
 import sqlite3
 
 
+if __name__ == "__main__":
+
+#Importing the songs_df from the Extract.py
+    load_df=Extract.return_dataframe()
+    if(Transform.Data_Quality(load_df) == False):
+        raise ("Failed at Data Validation")
