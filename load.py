7f068bc60506b6e04dc02235b45b10821f3f83b1
Joel Obadiah <joelobadiahdelle@gmail.com>
2024-11-24 17:58:40 +0100

    progress

--- load.py
+++ load.py
@@ -1,5 +1,5 @@
-import Extract
-import Transform
+import extract
+import transform
 import sqlalchemy
 import pandas as pd 
 from sqlalchemy.orm import sessionmaker
@@ -9,10 +9,16 @@ from datetime import datetime
 import datetime
 import sqlite3
 
+DATABASE_LOCATION = "sqlite:///my_played_tracks.sqlite"
 
 if __name__ == "__main__":
 
 #Importing the songs_df from the Extract.py
-    load_df=Extract.return_dataframe()
-    if(Transform.Data_Quality(load_df) == False):
+    load_df=extract.return_dataframe()
+    if(transform.Data_Quality(load_df) == False):
         raise ("Failed at Data Validation")
+
+engine = sqlalchemy.create_engine(DATABASE_LOCATION)
+conn = sqlite3.connect('my_played_tracks.sqlite')
+cursor = conn.cursor()
+
